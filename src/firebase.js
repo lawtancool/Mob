0510fb68b515e8511a1cc3b9c3cceffdab448376
lawtancool <26829131+lawtancool@users.noreply.github.com>
2021-10-23 16:49:41 -0700

    Add firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdeAChS8AHur-H7FQwLAtUcEnaYlQXKlE",
+  authDomain: "mob-dubhacks21.firebaseapp.com",
+  projectId: "mob-dubhacks21",
+  storageBucket: "mob-dubhacks21.appspot.com",
+  messagingSenderId: "866694741309",
+  appId: "1:866694741309:web:96e08a5a384758ee609954"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getFirestore();
