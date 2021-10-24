eb3a86b62f369324712213dd1cf92d6c5dbfdc5a
lawtancool <26829131+lawtancool@users.noreply.github.com>
2021-10-23 18:51:37 -0700

    Fix map.js

--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Router, Switch, Route, Link, useHistory } from 'react-router-dom';
+import { Router, Switch, Route, Link, useHistory, useParams } from 'react-router-dom';
 
-const Lobby = (props) => {
+const Map = (props) => {
+
+    const params = useParams();
+    const lobbyId = params.lobby_id;
     return (
         <div className='insert_class_name_here'>
-            <h1>lobby</h1> 
+            <h1>Map for lobby {lobbyId}</h1> 
         </div>
     );
 }
 
-export default Lobby;
+export default Map;
